c9a8b8138126786f81963220aa6ea07651619102
amirrezacar <68778340+amirrezacar@users.noreply.github.com>
2020-09-09 16:05:16 +0430

    Update moasese.py

--- moasese.py
+++ moasese.py
@@ -1,39 +1,46 @@
-import tkinter as tk 
-from  tkinter import Frame
+from time import sleep
+import tkinter as tk
+from tkinter import Frame
 import tkinter.ttk as ttk
+from threading import Thread
 
 
-def start():
-    pass
+def time_format(seconds):
+    h = int(seconds / 3600)
+    # m = 
+    # s = 
+    return '%02d:%02d:%02d'%(h, m, s)
+
+def counter(seconds):
+    while seconds:
+        sleep(1)
+        seconds -= 1
+        print(seconds)
+
+def start(number):
+    if number == 1:
+        seconds1 = int(h_p_1.get()) * 3600 + int(m_p_1.get()) * 60 + int(s_p_1.get())
+        th1 = Thread(target=counter, args=(seconds1,))
+        th1.start()
 
 def callback1(arg1, arg2, arg3):
     p1.set(n_p_1.get())
-    
+ 
 def callback2(arg1, arg2, arg3):
     p2.set(n_p_2.get())
-
+   
 def callback3(arg1, arg2, arg3):
     p3.set(n_p_3.get())
 
-
 def callback_t_1(arg1, arg2, arg3):
-    t1.set('%02d:%02d:%02d'%(int(h_p_1.get()),int(m_p_1.get()),int(s_p_1.get())))
+    t1.set('%02d:%02d:%02d'%(int(h_p_1.get()), int(m_p_1.get()), int(s_p_1.get())))
 
 def callback_t_2(arg1, arg2, arg3):
-    t1.set('%02d:%02d:%02d'%(int(h_p_2.get()),int(m_p_2.get()),int(s_p_2.get())))
+    t2.set('%02d:%02d:%02d'%(int(h_p_2.get()), int(m_p_2.get()), int(s_p_2.get())))
 
 def callback_t_3(arg1, arg2, arg3):
-    t1.set'%02d:%02d:%02d'%(int(h_p_3.get()),int(m_p_3.get()),int(s_p_3.get())))
-
-
-
-
-
-
-
-
-
-
+    t3.set('%02d:%02d:%02d'%(int(h_p_3.get()), int(m_p_3.get()), int(s_p_3.get())))
+        
 root = tk.Tk()
 
 note = ttk.Notebook()
@@ -66,9 +73,9 @@ t3 = tk.StringVar()
 t3.set('00:00:00')
 tk.Label(timer, textvariable=t3).grid(row=1, column=2)
 # ########### Timer Third Row Buttons ########## #
-tk.Button(timer, text='Start', command=start).grid(row=2, column=0)
-tk.Button(timer, text='Start', command=start).grid(row=2, column=1)
-tk.Button(timer, text='Start', command=start).grid(row=2, column=2)
+tk.Button(timer, text='Start', command=lambda: start(1)).grid(row=2, column=0)
+tk.Button(timer, text='Start', command=lambda: start(2)).grid(row=2, column=1)
+tk.Button(timer, text='Start', command=lambda: start(3)).grid(row=2, column=2)
 # ######## Timer Last Row Cancel Button ######## #
 tk.Button(timer, text='Cancel', command=root.destroy).grid(row=3, column=0, columnspan=3, sticky=tk.E+tk.W)
 # ############ Patient 1 Name Timer ############ #
@@ -77,6 +84,7 @@ patient1.grid(row=0, column=0, padx=10)
 
 tk.Label(patient1, text='Name').grid(row=0, column=0)
 tk.Label(patient1, text='Timer').grid(row=1, column=0)
+# ------------------Name--------------------- #
 n_p_1 = tk.StringVar()
 n_p_1.trace('w', callback1)
 tk.Entry(patient1, textvariable=n_p_1).grid(row=0, column=1)
@@ -89,26 +97,11 @@ s_p_1 = tk.StringVar()
 s_p_1.set('30')
 f1 = tk.Frame(patient1)
 f1.grid(row=1, column=1)
-tk.Spinbox(f1,
-           from_=0,
-           to=23,
-           wrap=True,
-           textvariable=h_p_1,
-           width=2,
+tk.Spinbox(f1, from_=0, to=23, wrap=True, textvariable=h_p_1, width=2,
            state="readonly").grid(row=0, column=0)
-tk.Spinbox(f1,
-           from_=0,
-           to=59,
-           wrap=True,
-           textvariable=m_p_1,
-           width=2,
+tk.Spinbox(f1, from_=0, to=59, wrap=True, textvariable=m_p_1, width=2,
            state="readonly").grid(row=0, column=1)
-tk.Spinbox(f1,
-           from_=0,
-           to=59,
-           wrap=True,
-           textvariable=s_p_1,
-           width=2,
+tk.Spinbox(f1, from_=0, to=59, wrap=True, textvariable=s_p_1, width=2,
            state="readonly").grid(row=0, column=2)
 # ############ Patient 2 Name Timer ############ #
 patient2 = tk.LabelFrame(patient, text='Patient2')
@@ -116,6 +109,7 @@ patient2.grid(row=1, column=0, padx=10)
 
 tk.Label(patient2, text='Name').grid(row=0, column=0)
 tk.Label(patient2, text='Timer').grid(row=1, column=0)
+# ------------------Name--------------------- #
 n_p_2 = tk.StringVar()
 n_p_2.trace('w', callback2)
 tk.Entry(patient2, textvariable=n_p_2).grid(row=0, column=1)
@@ -126,36 +120,31 @@ m_p_2 = tk.StringVar()
 m_p_2.set('30')
 s_p_2 = tk.StringVar()
 s_p_2.set('30')
-f1 = tk.Frame(patient2)
-f1.grid(row=1, column=1)
-tk.Spinbox(f1,
-           from_=0,
-           to=23,
-           wrap=True,
-           textvariable=h_p_2,
-           width=2,
+f2 = tk.Frame(patient2)
+f2.grid(row=1, column=1)
+tk.Spinbox(f2, from_=0, to=23, wrap=True, textvariable=h_p_2, width=2,
            state="readonly").grid(row=0, column=0)
-tk.Spinbox(f1,
+tk.Spinbox(f2,
            from_=0,
            to=59,
            wrap=True,
            textvariable=m_p_2,
            width=2,
            state="readonly").grid(row=0, column=1)
-tk.Spinbox(f1,
+tk.Spinbox(f2,
            from_=0,
            to=59,
            wrap=True,
            textvariable=s_p_2,
            width=2,
            state="readonly").grid(row=0, column=2)
-
 # ############ Patient 3 Name Timer ############ #
 patient3 = tk.LabelFrame(patient, text='Patient3')
 patient3.grid(row=2, column=0, padx=10)
 
 tk.Label(patient3, text='Name').grid(row=0, column=0)
 tk.Label(patient3, text='Timer').grid(row=1, column=0)
+# ------------------Name--------------------- #
 n_p_3 = tk.StringVar()
 n_p_3.trace('w', callback3)
 tk.Entry(patient3, textvariable=n_p_3).grid(row=0, column=1)
@@ -166,30 +155,30 @@ m_p_3 = tk.StringVar()
 m_p_3.set('30')
 s_p_3 = tk.StringVar()
 s_p_3.set('30')
-f1 = tk.Frame(patient3)
-f1.grid(row=1, column=1)
-tk.Spinbox(f1,
+f3 = tk.Frame(patient3)
+f3.grid(row=1, column=1)
+tk.Spinbox(f3,
            from_=0,
            to=23,
            wrap=True,
            textvariable=h_p_3,
            width=2,
            state="readonly").grid(row=0, column=0)
-tk.Spinbox(f1,
+tk.Spinbox(f3,
            from_=0,
            to=59,
            wrap=True,
            textvariable=m_p_3,
            width=2,
            state="readonly").grid(row=0, column=1)
-tk.Spinbox(f1,
+tk.Spinbox(f3,
            from_=0,
            to=59,
            wrap=True,
            textvariable=s_p_3,
            width=2,
            state="readonly").grid(row=0, column=2)
-
+# --------------- Timer Callbacks -------------- #
 h_p_1.trace('w', callback_t_1)
 m_p_1.trace('w', callback_t_1)
 s_p_1.trace('w', callback_t_1)
@@ -199,13 +188,4 @@ s_p_2.trace('w', callback_t_2)
 h_p_3.trace('w', callback_t_3)
 m_p_3.trace('w', callback_t_3)
 s_p_3.trace('w', callback_t_3)
-
-
-
-
-
-
-
-
 root.mainloop()
-
